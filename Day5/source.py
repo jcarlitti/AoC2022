4f4f5104857725e7783b2768724e96a87ecc3b7e
Justin Carlitti <JustinCarlitti@go.rmc.edu>
2022-12-06 20:22:12 -0500

    Update source.py

--- Day5/source.py
+++ Day5/source.py
@@ -1,46 +1,46 @@
-from collections import deque
-import re
+def parseData():
+  with open("text.txt", "r") as f:
+    data = f.read()
+  data = data.split("\n\n")
+  chart = data[0].splitlines()[:-1] 
+  instr = data[1].splitlines()
+  return chart, instr
 
-def driver():
-  stack = dict([(key, []) for key in range(1,10)])
-  data, instr = parseData(stack)
-  #print(partOne(data, instr)) #Uncomment to run partOne
-  print(partTwo(data, instr))
-
-def parseData(stack):
-  with open("text.txt") as f:
-    data = f.read().split("\n\n")
-    chart = data[0].splitlines()[:-1]
-    instructions = data[1].splitlines()
+def partOne(chart, instr):
+  stack = dict([(key, []) for key in range(1, 10)])
   for row in chart:
     for j in range(1, len(row), 4):
       pos = j // 4 + 1
-      if row[j] != " ":
+      if row[j] != ' ':
         stack[pos].append(row[j])
-  return stack, instructions
-
-def partOne(stack, instr):
   for line in instr:
-    loc = line.rstrip().split(" ")
-    for i in range(0, int(loc[1])):
-      stack[int(loc[5])].insert(0, stack[int(loc[3])].pop(0))
-  #return ''.join([value[len(value)-1] for (key, value) in stack.items()]) 
-  finalStr = ""
+    line = [int(i) for i in line.split() if i.isdigit()]
+    for i in range(0, line[0]):
+      stack[line[2]].insert(0, stack[line[1]].pop(0))
+  final_str = ""
   for key in stack.keys():
-    finalStr += stack[key][0]
-  return finalStr
+    final_str += stack[key][0] 
+  return final_str
 
-def partTwo(stack, instr):
+def partTwo(chart, instr):
+  stack = dict([(key, []) for key in range(1,10)])
+  for row in chart:
+    for j in range(1, len(row), 4):
+      pos = j // 4 + 1
+      if(row[j] != " "):
+        stack[pos].append(row[j])
   for line in instr:
-    loc = line.rstrip().split(" ")
-    stack[int(loc[5])] = stack[int(loc[3])][:int(loc[1])]+stack[int(loc[5])]
-    stack[int(loc[3])] = stack[int(loc[3])][int(loc[1]):]
-  finalStr = ""
+    line = [int(i) for i in line.split() if i.isdigit()]
+    stack[line[2]] = stack[line[1]][:line[0]] + stack[line[2]]
+    stack[line[1]] = stack[line[1]][line[0]:]
+  final_str = ""
   for key in stack.keys():
-    finalStr += stack[key][0]
-  return finalStr
+    final_str += stack[key][0]
+  return final_str
 
 def main():
-  driver()
+  chart, instr = parseData()
+  print(partOne(chart, instr))
+  print(partTwo(chart, instr))
 
 main()
